cfa8b9822aa9a61503d99cedd6f91dce8b5c1816
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-05-31 13:49:57 +0300

    add website content

---
+++ script/index.js
@@ -0,0 +1,16 @@
+const headerTitle = document.getElementById("header-title");
+headerTitle.addEventListener('mouseover', function () {
+  headerTitle.style.color = "magenta";
+});
+headerTitle.addEventListener('mouseout', function () {
+  headerTitle.style.color = "";
+});
+
+
+const foot = document.getElementById("footer");
+foot.addEventListener('mouseover', function () {
+  foot.style.color = "magenta";
+});
+foot.addEventListener('mouseout', function () {
+  foot.style.color = "";
+});
